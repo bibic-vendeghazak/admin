5e6438d7139af3b5e189d99ded81a4e671c694a0
Balázs Orbán <info@balazsorban.com>
2018-01-27 17:51:15 +0100

    :art: :tractor: :sparkles: Add delete dialog

--- src/components/Reservations/Reservation.jsx
+++ src/components/Reservations/Reservation.jsx
@@ -1,20 +1,20 @@
 import React, {Component} from 'react'
-  import firebase from 'firebase'
+import firebase from 'firebase'
 import moment from 'moment'
-  import {List, ListItem} from 'material-ui/List'
-  import {ExpandableCard} from '../shared'
-  import {
-    Table,
-    TableBody,
-    TableHeaderColumn,
-    TableRow,
-    TableRowColumn,
-  } from 'material-ui/Table'
+import {List, ListItem} from 'material-ui/List'
+import {ExpandableCard} from '../shared'
+import {
+  Table,
+  TableBody,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table'
 import FontIcon from 'material-ui/FontIcon'
 import Edit from 'material-ui/svg-icons/image/edit'
-  import CommunicationCall from 'material-ui/svg-icons/communication/call'
-  import Email from 'material-ui/svg-icons/communication/email'
-  import Message from 'material-ui/svg-icons/communication/message'
+import CommunicationCall from 'material-ui/svg-icons/communication/call'
+import Email from 'material-ui/svg-icons/communication/email'
+import Message from 'material-ui/svg-icons/communication/message'
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
@@ -24,22 +24,26 @@ export default class Reservation extends Component {
 
   state= {
     isDeleteDialogOpen: false
-    }
+  }
 
-    const toDate = date => {
-      return new Date(date).toISOString().slice(0,10)
-    }
-
-    const dayDiff = (timestamp1, timestamp2) => {
-      var difference = timestamp1 - timestamp2
-      var daysDifference = Math.floor(difference/1000/60/60/24)
-      return daysDifference
-    }
+  handleReservation = isHandled => {
+    firebase.database().ref(`reservations/${this.props.id}/metadata/handled`).set(isHandled)
+  }
 
 
+  openDeleteReservationDialog = () => this.setState({isDeleteDialogOpen: true})
+  closeDeleteReservationDialog = () => this.setState({isDeleteDialogOpen: false})
 
+  deleteReservation = () => {
+    firebase.database().ref(`reservations/${this.props.id}`).remove()
+  }
+
+
+  render() {
+    const {details: {name, email, tel, message, adults, children}, metadata: {roomId, from, to, handled}} = this.props.reservation
+    const {isDeleteDialogOpen} = this.state
     return (
-      <ListItem disabled style={{padding: ".25em 4em"}}>
+      <ListItem disabled style={{padding: ".25em 5vw"}}>
       <ExpandableCard
         title={`${name} (${moment(to).diff(moment(from), "days")} nap)`}
         subtitle={`Szoba ${roomId}`}
@@ -83,20 +87,39 @@ export default class Reservation extends Component {
               style={{marginRight: 12}}
               primary
               label="Elfogadás"
-              onClick={() => handleReservation(true)}
+              onClick={() => this.handleReservation(true)}
             />
             }
             <RaisedButton
               secondary
-              label={handled ? "Visszavon" :"Elutasítás"}
-              onClick={() => handleReservation(false)}
+              label={handled ? "Visszavon" : "Törlés"}
+              onClick={() => handled ? this.handleReservation(false) : this.openDeleteReservationDialog()}
             />
+              <Dialog
+                title="Foglalás végleges törlése"
+                actions={[
+                  <FlatButton
+                    label="Mégse"
+                    primary
+                    onClick={this.closeDeleteReservationDialog}
+                  />,
+                  <RaisedButton
+                    label="Végleges törlés"
+                    secondary
+                    onClick={this.deleteReservation}
+                  />,
+                ]}
+                modal
+                open={isDeleteDialogOpen}
+              >
+              Biztos törölni szeretné ezt a foglalást? A folyamat nem visszafordítható!
+            </Dialog>
           </div>
       </div>
       </ExpandableCard>
     </ListItem>
-
     )
+    
   }
 
-  export default Reservation
+}
