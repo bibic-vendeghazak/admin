db16e0500060d9d1acf141479597e026df9eaa9a
Balázs Orbán <info@balazsorban.com>
2018-01-31 01:58:52 +0100

    :pencil: :ambulance: Fix typo

--- src/components/App.js
+++ src/components/App.js
@@ -164,7 +164,7 @@ export default class App extends Component {
               {{
                 error: <Error color={red}/>,
                 warning: <Warning color={yellow}/>,
-                success: <Success color={green}>
+                success: <Success color={green}/>
               }[type]}
             </div>
             }
