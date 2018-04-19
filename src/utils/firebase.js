254a51db4bad84ee4fd3fef1b58bd239cc10f8b2
Balázs Orbán <info@balazsorban.com>
2018-04-19 12:23:25 +0200

    🔧 add firebase helper functions to utils

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-
+import "firebase/firestore"
 firebase.initializeApp({
   apiKey: "AIzaSyB4-Y2_RCdrOouJJxUJkBBXGyj4hNdjDs0",
   authDomain: "bibic-vendeghazak-api.firebaseapp.com",
@@ -8,3 +8,6 @@ firebase.initializeApp({
   storageBucket: "bibic-vendeghazak-api.appspot.com",
   messagingSenderId: "586582307718"
 })
+
+export const fs = firebase.firestore()
+export const db = firebase.database()
