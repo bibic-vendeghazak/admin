ff3efb0ae23242a30e4a754ad6a2a3d45d63693a
Balázs Orbán <info@balazsorban.com>
2018-08-07 15:41:17 +0200

    💄 UI changes
    Better use of Material Design

--- src/components/scenes/Feedbacks/Feedback.jsx
+++ src/components/scenes/Feedbacks/Feedback.jsx
@@ -3,20 +3,22 @@ import moment from "moment"
 import {DB, ADMINS, AUTH, FEEDBACKS_DB} from "../../../utils/firebase"
 
 import {ListItem} from "material-ui/List"
-import {Card, CardActions, CardText} from "material-ui/Card"
-import RaisedButton from "material-ui/RaisedButton"
-import Done from "material-ui/svg-icons/action/done"
-import Delete from "material-ui/svg-icons/action/delete"
+import {Card, CardActions, CardText, CardTitle} from "material-ui/Card"
+import {Divider, FlatButton} from "material-ui"
+import {ModalDialog} from "../../shared"
 
 export default class Feedback extends Component {
 
-  state = {adminName: ""}
+  state = {
+    adminName: null,
+    isDelete: false,
+    isSubmit: false
+  }
 
   componentDidMount() {
     DB.ref(`feedbacks/${this.props.feedbackId}/lastHandledBy`).on("value", snap => {
-      if (snap.val() !== "") {
-        ADMINS.child(
-          snap.val())
+      if (snap.exists() && snap.val() !== "") {
+        ADMINS.child(snap.val())
           .on("value", snap => snap.val() && this.setState({adminName: snap.val().name}))
       }
     })
@@ -26,67 +28,75 @@ export default class Feedback extends Component {
     const feedbackRef = DB.ref(`feedbacks/${this.props.feedbackId}`)
     feedbackRef.update({"handled": true})
     feedbackRef.child("lastHandledBy").set(AUTH.currentUser.uid)
+    this.handleCloseDialog()
   }
 
   handleDeleteFeedback = () => {
     FEEDBACKS_DB.child(this.props.feedbackId).remove()
+    this.handleCloseDialog()
   }
 
+  handleCloseDialog = () => this.setState({
+    isDelete: false,
+    isSubmit: false
+  })
+
+  handleOpenDialog = type => this.setState({[type]: true})
 
   render() {
     const {feedback: {
       handled, message, timestamp
     }} = this.props
-    const {adminName} = this.state
+    const {
+      adminName, isDelete, isSubmit
+    } = this.state
     return (
-      <ListItem
-        disabled
-        style={{padding: ".5em 5vw"}}
-      >
-        <Card
-          style={{position: "relative"}}
-        >
-          <p style={{
-            padding: "1em",
-            color: "#ccc",
-            fontSize: ".8em",
-            textAlign: "right",
-            fontStyle: "italic"
-          }}
+      <ListItem disabled>
+        <ModalDialog
+          onCancel={this.handleCloseDialog}
+          onSubmit={this.handleMarkAccepted}
+          open={isSubmit}
+          title="Biztos jóváhagyja?"
+        />
+        <ModalDialog
+          onCancel={this.handleCloseDialog}
+          onSubmit={this.handleDeleteFeedback}
+          open={isDelete}
+          title="Biztos törölni szeretné?"
+        />
+        <Card>
+          <CardTitle
+            actAsExpander
+            showExpandableButton
+            subtitle={`jóváhagyta • ${adminName || "Még senki" }`}
+            title={`${message.split(" ").slice(0, 3).join(" ")}...`}
           >
-            {adminName !== "" && `jóváhagyta: ${adminName}`} <br/>
-          </p>
-          <CardText>
+          </CardTitle>
+          <CardText expandable>
             {message}
+            <p style={{
+              color: "grey",
+              fontSize: 12
+            }}
+            >
+              {moment(timestamp).format("YYYY. MMMM DD. HH:mm")}
+            </p>
           </CardText>
-          <CardActions >
+          <Divider/>
+          <CardActions>
             {!handled &&
-                <RaisedButton
-                  icon={<Done/>}
-                  label="Jóváhagyás"
-                  labelPosition="before"
-                  onClick={this.handleMarkAccepted}
-                  primary
+                <FlatButton
+                  label="Jóváhagy"
+                  onClick={() => this.handleOpenDialog("isSubmit")}
                 />
             }
-            <RaisedButton
-              icon={<Delete/>}
-              label="Törlés"
-              labelPosition="before"
-              onClick={this.handleDeleteFeedback}
+            <FlatButton
+              label="Töröl"
+              onClick={() => this.handleOpenDialog("isDelete")}
               secondary
             />
           </CardActions>
-          <p style={{
-            padding: "1em",
-            color: "#ccc",
-            fontSize: ".8em",
-            textAlign: "right",
-            fontStyle: "italic"
-          }}
           >
-                beküldve: {moment(timestamp).format("YYYY. MMMM DD. HH:mm")}
-          </p>
         </Card>
       </ListItem>
     )
