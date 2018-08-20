a9d0d18c1cf4119941c1b86bcffa8e822b354ad5
Balázs Orbán <info@balazsorban.com>
2018-08-20 18:03:39 +0200

    🔥 remove legacy material-ui

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {BrowserRouter as Router} from "react-router-dom"
+import {MuiThemeProvider as Material} from '@material-ui/core/styles'
+import {Database} from "./components/App/Store"
 
-import {MuiThemeProvider as Material2} from '@material-ui/core/styles'
-import {MuiThemeProvider} from "material-ui/styles"
 
-
-import {muiTheme, mui2Theme} from "./utils"
+import {theme} from "./utils"
 import "./utils/moment"
 import "./utils/firebase"
-import "./styles/main.css"
 import App from "./components/App"
 
 ReactDOM.render(
-  <Material2 theme={mui2Theme}>
-    <MuiThemeProvider
-      children={
-        <Router>
-          <App/>
-        </Router>
-      }
-      muiTheme={muiTheme}
-    />
-  </Material2>,
+  <Material theme={theme}>
+    <Database>
+      <Router>
+        <App/>
+      </Router>
+    </Database>
+  </Material>,
   document.querySelector("#root")
 )
