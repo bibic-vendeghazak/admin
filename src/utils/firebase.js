631ad0399d30a21130e5f0e3d78acde9e69b30de
Balázs Orbán <info@balazsorban.com>
2018-08-26 18:58:56 +0200

    :pencil: minor changes:

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,8 +1,8 @@
-import firebase from "firebase/app"
-import "firebase/database"
-import "firebase/firestore"
-import "firebase/auth"
-import "firebase/storage"
+import firebase from "@firebase/app"
+import "@firebase/database"
+import "@firebase/firestore"
+import "@firebase/auth"
+import "@firebase/storage"
 
 firebase.initializeApp({
   apiKey: "AIzaSyB4-Y2_RCdrOouJJxUJkBBXGyj4hNdjDs0",
