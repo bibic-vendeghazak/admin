eea8e1dfd28536af65bec837213eeb87e5f3cacc
Balázs Orbán <info@balazsorban.com>
2018-11-06 19:55:01 +0100

    👌 changes due to testing
     #3

--- src/utils/moment.js
+++ src/utils/moment.js
@@ -1,10 +1,11 @@
-import Moment from "moment"
+import moment from "moment"
 import "moment/locale/hu"
 import {extendMoment} from "moment-range"
-export default extendMoment(Moment)
 
 /*
- * This file is related to moment.js
+* This file is related to moment.js
  * Hungarian locale added
  * Extending moment.js with moment-range
  */
+
+export default extendMoment(moment)
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,6 @@
 import {createMuiTheme} from "@material-ui/core/styles"
 import colors from "./colors"
 
-export default createMuiTheme({palette: {
-  primary: {main: colors.lightBrown},
-  secondary: {main: colors.orange}
-}})
+export default createMuiTheme({palette: {primary: {main: colors.lightBrown},
+  secondary: {main: colors.orange}},
+typography: {useNextVariants: true}})
