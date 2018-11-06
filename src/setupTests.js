e3b28308b4486078f59c655d6f1bd1e82ab7cda9
Balázs Orbán <info@balazsorban.com>
2018-11-06 19:54:06 +0100

    ➕ adding enzyme
     #3

---
+++ src/setupTests.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {
+  configure, mount, shallow
+} from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+configure({adapter: new Adapter()})
+import {moment} from "./utils"
+
+/**
+ * make these available without needing to import them every time in tests
+ * @see https://youtu.be/uo0psyTxgQM?t=903
+ */
+global.React = React
+global.mount = mount
+global.shallow = shallow
+global.moment = moment
