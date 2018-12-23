311ff8ec574c1d7206a18a5c43ba224335eb835c
Balázs Orbán <info@balazsorban.com>
2018-12-23 11:03:16 +0100

    🚚 moving Cloud Functions into own repo
    https://github.com/bibic-vendeghazak/cloud-functions #14

--- functions/src/lib/feedbacks/utils.js
+++
@@ -1,4 +0,0 @@
-export const getRatingAverage = ratings => {
-  ratings = Object.values(ratings)
-  return ratings.reduce((acc, rating) => acc + rating) / ratings.length
-}
--- functions/src/lib/moment.js
+++
@@ -1,5 +0,0 @@
-// Init moment.js
-import moment from "moment"
-import { extendMoment } from "moment-range"
-moment.locale("hu")
-export default extendMoment(moment)
